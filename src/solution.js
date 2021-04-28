400e334dc880d3c7a7ef05dc4179d47b26627938
Wasswa Richard <wasswarichard55@gmail.com>
2021-04-28 11:00:11 +0300

    enhancement

--- src/solution.js
+++ src/solution.js
@@ -23,9 +23,7 @@ export const prepareData = (filterParams,responseData) => {
         })
         .slice().sort((a, b) => b.payloads_count - a.payloads_count || new Date(b.launch_date_utc).getTime() - new Date(a.launch_date_utc).getTime())
         .forEach( data => {
-            const selectedParameters = {}
-            Object.assign(selectedParameters, _.pick(data, ["flight_number", "mission_name", "payloads_count"]))
-            filterData.push(selectedParameters);
+            filterData.push({..._.pick(data, ["flight_number", "mission_name", "payloads_count"])});
         });
     return filterData
 }
