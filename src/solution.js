865f712298d016219e09468b1b00a17564b66f52
Wasswa Richard <wasswarichard55@gmail.com>
2021-04-29 22:51:24 +0300

    enhancement

--- src/solution.js
+++ src/solution.js
@@ -15,11 +15,7 @@ export const prepareData = (filterParams,responseData) => {
         .filter(data =>  data.launch_year === filterParams.year.toString() && checkMissionPayload(data.rocket.second_stage.payloads))
         .map(launch => {
             const payloads_count = launch.rocket.second_stage.payloads.length;
-            return {
-                ...launch,
-                launch_date_utc : new Date(launch.launch_date_utc).toUTCString(),
-                payloads_count
-            }
+            return {...launch, launch_date_utc : new Date(launch.launch_date_utc).toUTCString(), payloads_count}
         })
         .slice().sort((a, b) => b.payloads_count - a.payloads_count || new Date(b.launch_date_utc).getTime() - new Date(a.launch_date_utc).getTime())
         .forEach( data => filterData.push({..._.pick(data, ["flight_number", "mission_name", "payloads_count"])}));
